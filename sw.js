95cb47fe7fc6d0403eb90971c47a6bfc4e6374a6
aislantavares329-eng <aislantavares329@gmail.com>
2026-08-15 17:33:14 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE="controle-alimentos-v2";
-const ASSETS=["./","./index.html","./style.css","./app.js","./manifest.json","./icon-192.png","./icon-512.png"];
+const CACHE="controle-alimentos-v3";
+const ASSETS=["./","./index.html","./style.css","./app.js","./manifest-v3.json","./icon-192.png","./icon-512.png"];
 self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
 self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
